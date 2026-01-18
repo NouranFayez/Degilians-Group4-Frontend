ded9b14cbd3949bee7fb9bfbb7bc8fb6123a2ed9
NouranFayez <nouran_ahmed269@hotmail.com>
2026-01-18 11:19:07 +0200

    session 8

---
+++ session8/js/script.js
@@ -0,0 +1,148 @@
+console.log('javascript')
+
+// var - let - const 
+
+var firstName = "Nouran"
+var age = 31
+var isGraduated = true
+var lastName
+
+var test = null
+
+// var p = prompt("enter your name" , "username")
+
+
+// console.log(firstName)
+// console.log(lastName)
+
+// console.log(p)
+
+var n = 20
+var m = 30
+
+var sum = n + m
+
+
+// console.log(sum)
+// console.log(4**2)
+
+// var h = 20
+
+// console.log(h++) // 20
+// console.log(h) // 21
+
+// console.log(++h)
+
+// decrement -- 
+
+
+// comparison Operator ==> (> , >= , < , <= , == , === , != , !==)
+
+
+var a = 20
+var b = 20
+
+// console.log(a > b)
+// console.log(a >= b)
+
+var h = 20
+var k = "20"
+
+// console.log(h == k) // value
+// console.log(h === k) // value - data type
+
+// console.log(h != k) // value
+// console.log(h !== k) // value - data type
+
+
+var fName = "Ahmed"
+var userAge = 20
+
+//   logical operators ==> (&& || !)
+// console.log(fName == "Ahmed")
+// console.log(userAge == 30)
+
+// console.log(fName == 'Ahmed' && userAge == 30 )
+// console.log(fName == 'Ahmed' && userAge > 10 )
+
+// console.log(fName == "Mohamed" || userAge == 20)
+
+
+// console.log(!(fName == "vbnm"))
+
+// console.log(typeof(fName))
+// console.log(typeof userAge)
+
+// console.log(typeof userAge == "number")
+// console.log(typeof userAge == number)
+// console.log(typeof userAge == Number)
+
+
+//  (condition) ? true : false
+
+// var userAge = 20;
+// (typeof userAge == "number") ? console.log('number') : console.log('invalid')
+
+
+// truthy anf false value
+var num = 20;// true
+var num = 0; // false
+var num = -0; // false
+// var num = -444; // false
+// var num = ""; // false
+// var num = null; // false
+// var num = "dfghjk"; // true
+// var num = true; // true
+// var num = false; // true
+
+var address; // false
+
+// (num) ? console.log('correct') : console.log('wrong')
+(address) ? console.log('correct') : console.log('wrong');
+
+
+// if(condition/s) {
+//  true 
+// }
+
+// else {
+//     false
+// }
+
+var fName = prompt('enter your name')
+
+console.log(fName.toLowerCase())
+console.log(fName.toUpperCase())
+
+
+
+if (fName.toLowerCase() == "ali") {
+    var result = fName
+    console.log(result)
+    console.log('Developer')
+}
+else if(fName.toLowerCase() == "mohamed"){
+    console.log('Accountant')
+}
+else if (fName.toLowerCase() == "ahmed"){
+    console.log('Engineer')
+}
+else {
+    console.log('invalid user')
+}
+
+
+
+degree = prompt('enter your degree')
+
+
+
+
+
+
+
+
+
+
+
+
