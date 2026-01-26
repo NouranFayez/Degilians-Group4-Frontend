9c0494612d51c91301a2ee397dfcd71be2ffe236
NouranFayez <nouran_ahmed269@hotmail.com>
2026-01-26 09:43:23 +0200

    session11

--- session10/js/script.js
+++ session10/js/script.js
@@ -1,4 +1,4 @@
-var ids = [2, 10, 5 , 6]
+var ids = [2, 10, 5, 6]
 // console.log(ids[0])
 // console.log(ids[1])
 // console.log(ids[2])
@@ -8,7 +8,7 @@ var ids = [2, 10, 5 , 6]
 //     console.log(ids[i])
 // }
 
-var ids = [2, 10, 5 , 6]
+var ids = [2, 10, 5, 6]
 // special function
 // ids.forEach((ele , ind , array)=>{
 //     // console.log(ele)
@@ -21,25 +21,22 @@ var ids = [2, 10, 5 , 6]
 
 // }
 
-var ids = [2, 7 ,  10, 5 , 6]
+var ids = [2, 7, 10, 5, 6]
 
 
 // return item
-var ele = ids.find((item)=> item >= 6)
-var ele = ids.find((item)=> item >= 20) // undefined
-    // console.log(ele)
-
-
-
- var index = ids.findIndex(item => item > 6)   
- var index = ids.findIndex(item => item > 20)  // -1
+var ele = ids.find((item) => item >= 6)
+var ele = ids.find((item) => item >= 20) // undefined
+// console.log(ele)
+var index = ids.findIndex(item => item > 6)
+var index = ids.findIndex(item => item > 20)  // -1
 //  console.log(index)
 // ()=>{
 
 // }
 
-var items = ids.filter(item => item> 5)
-var items = ids.filter(item => item> 50)
+// var items = ids.filter(item => item> 5)
+// var items = ids.filter(item => item> 50)
 
 
 // console.log(items)
@@ -50,22 +47,22 @@ var items = ids.filter(item => item> 50)
 // var user = ['Nouran' , "Ahmed" , "Fayez" ,32 , "developer"]
 
 var user = {
-    firstName : "Nouran",
-    lastName : "Fayez",
-    age : 32 , 
-    getFullName : function(){
-       return `${this.firstName} ${this.lastName}`
+    firstName: "Nouran",
+    lastName: "Fayez",
+    age: 32,
+    getFullName: function () {
+        return `${this.firstName} ${this.lastName}`
     },
-    fullName : ()=>{
+    fullName: () => {
         // console.log(this) // window
         //  return `${this.firstName} ${this.lastName}`
     },
-    addressList : function(){
+    addressList: function () {
         return {
-            homeAddress : "Nasr city",
-            officeAddress : 'Maadi',
-            testAddress : {
-                address1 : 'October',
+            homeAddress: "Nasr city",
+            officeAddress: 'Maadi',
+            testAddress: {
+                address1: 'October',
                 address2: "New cairo"
             }
         }
@@ -75,7 +72,77 @@ var user = {
 // console.log(user.firstName)
 // console.log(user["age"])
 // console.log(user.getFullName())
-console.log(user.fullName())
-console.log(user.addressList().officeAddress)
-console.log(user.addressList().testAddress.address2)
+// console.log(user.fullName())
+// console.log(user.addressList().officeAddress)
+// console.log(user.addressList().testAddress.address2)
+
+var user1 = {
+    fName: "Ahmed",
+    lName: 'Mohamed',
+    age: 20
+}
+var user2 = {
+    fName: "Marwa",
+    lName: 'Mohamed',
+    age: 30
+}
+var users = [
+    {
+        fName: "Ahmed",
+        lName: 'Mohamed',
+        age: 20
+    },
+    {
+        fName: "Marwa",
+        lName: 'Mohamed',
+        age: 30
+    }
+]
+
+var ind = users.findIndex((item)=> item.age == 20)
+// ele.age = 25
+// console.log(ele)
+
+// console.log(users[ind].age = 40)
+
+// console.log(users)
+
+
+
+
+// name , id , balance
+
+// addUsers() ==> name , id , balance
+// editUserBalanceById() ==> id , newBalance
+// deleteUserById() ==> id
+
+if(localStorage.getItem('users')){
+    var users = JSON.parse(localStorage.getItem('users'))
+
+}
+else var users = []
+
+var addUsers = ()=>{
+    var userName = prompt('enter user name')
+    var userId = prompt('enter user id')
+    // var userBalance = prompt('enter user Balance')
+    var user = {
+        userName : userName ,
+        userId ,
+        userBalance : prompt('enter user Balance')
+    }
+    usersList.push(user)
+    console.table(usersList)
+}
+
+var usersNum = prompt('number of users')
+
+for(var i = 0 ; i< usersNum ; i++){
+    addUsers()
+}
+
+
+var usersList = [
+    {age : 20 , email:"email@hotmail.com"}
+]
 
---
+++ session11/js/script.js
@@ -0,0 +1,40 @@
+
+// localStorage.setItem("userName", "Nouran")
+
+var users = [
+    {
+        fName: "Ahmed",
+        LName: "Mohamed"
+    },
+    {
+        fName: "Marwa",
+        LName: "Mohamed"
+    },
+    {
+        fName: "Hazem",
+        LName: "Mohamed"
+    }
+]
+
+// localStorage.setItem('users' , JSON.stringify(users))
+
+// var ids = [1 , 3, 5]
+
+// console.log(users.toString())
+// console.log(JSON.stringify(users))
+
+
+// var newArray = JSON.parse(localStorage.getItem('users'))
+// newArray.push({fName : 'tamer' , lName:'Fayez'})
+// console.log(newArray)
+
+// localStorage.removeItem('userName')
+// console.log(JSON.parse(localStorage.getItem('users').push({fName : 'tamer' , lName:'Fayez'})));
+
+
+
+
+// sessionStorage.setItem('userName' , 'Ahmed')
+
+
+console.log(localStorage.getItem('address'));
